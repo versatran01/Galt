b8cf086b0eac2ebe6b8d09289dacc4cd22f4db82
Chao Qu <quchao@seas.upenn.edu>
2014-05-23 15:57:50 -0400

    bluefox2: use catkin and new driver 2.7.2

---
+++ drivers/mvIMPACT_acquire/mvIMPACT_acquire-x86_64-2.7.2/apps/Common/Info.h
@@ -0,0 +1,12 @@
+//-----------------------------------------------------------------------------
+#ifndef InfoH
+#define InfoH InfoH
+//-----------------------------------------------------------------------------
+
+#define COMPANY_NAME wxT("MATRIX VISION GmbH")
+#define COMPANY_WEBSITE wxT("http://www.matrix-vision.com")
+#define COMPANY_SUPPORT_MAIL wxT("mailto:support@matrix-vision.com")
+#define CURRENT_YEAR wxT("2014")
+#define VERSION_STRING wxT("2.7.2.1237")
+
+#endif // InfoH

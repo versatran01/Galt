3f4dd356c28058ffc4701a088ddf9eef7b57d40b
Gareth Cross <gcross.code@icloud.com>
2014-07-12 01:40:00 -0400

    Update CMakeLists

--- core/spec_calib/src/main.cpp
+++ core/spec_calib/src/main.cpp
@@ -14,9 +14,13 @@
 
 #include "mainwindow.hpp"
 
+ros::NodeHandlePtr nh;
+
 int main(int argc, char *argv[])
 {
   ros::init(argc, argv, "spec_calib");
+  nh = ros::NodeHandlePtr(new ros::NodeHandle("~"));
+  
   QApplication a(argc, argv);
   MainWindow w;
   w.show();

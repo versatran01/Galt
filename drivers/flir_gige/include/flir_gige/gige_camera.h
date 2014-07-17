910f93a040ed127da11e255e46c1594f11a87663
Chao Qu <quchao@seas.upenn.edu>
2014-07-17 00:51:56 -0400

    add some thread stuff

--- drivers/flir_gige/include/flir_gige/gige_camera.h
+++ drivers/flir_gige/include/flir_gige/gige_camera.h
@@ -9,6 +9,8 @@
 
 #include <string>
 #include <memory>
+#include <thread>
+#include <functional>
 
 #include <PvSystem.h>
 #include <PvDevice.h>
@@ -20,15 +22,18 @@
 #include <PvBuffer.h>
 #include <PvPipeline.h>
 
+#include <opencv2/core/core.hpp>
 
 namespace flir_gige {
 
+// Functor for free PvDevice
 struct FreeDevice {
   void operator() (PvDevice *device) const {
     PvDevice::Free(device);
   }
 };
 
+// Functor for free PvStream
 struct FreeStream {
   void operator() (PvStream *stream) const {
     PvStream::Free(stream);
@@ -41,19 +46,30 @@ class GigeCamera {
   GigeCamera(const GigeCamera&) = delete;  // No copy constructor
   GigeCamera& operator=(const GigeCamera&) = delete;  // No assignment operator
 
+  // Find and connect to device, create PvDevice, PvStream and PvPipeline
+  void Connect();
+  //
+  void Configure();
+  //
+  void Start();
+  void Stop();
+  void Disconnect();
+
+  std::function<void(const cv::Mat &image)> use_image;
+
+ private:
   void FindDevice();
   void ConnectDevice();
   void OpenStream();
   void ConfigureStream();
   void CreatePipeline();
   void AcquireImages();
+  void ImageThread();
 
- private:
-//  typedef std::unique_ptr<PvDevice, decltype(PvDevice::Free)*> PvDeviceUPtr;
-//  typedef std::unique_ptr<PvStream, decltype(PvStream::Free)*> PvStreamUPtr;
   typedef std::unique_ptr<PvDevice, FreeDevice> PvDevicePtr;
   typedef std::unique_ptr<PvStream, FreeStream> PvStreamPtr;
   typedef std::unique_ptr<PvPipeline> PvPipelinePtr;
+  typedef std::unique_ptr<std::thread> ThreadPtr;
 
   std::string ip_address_;
   PvSystem system_;
@@ -61,6 +77,7 @@ class GigeCamera {
   PvDevicePtr device_;
   PvStreamPtr stream_;
   PvPipelinePtr pipeline_;
+  ThreadPtr image_thread_;
 };  // class GigeCamera
 
 }  // namespace flir_gige
--- drivers/flir_gige/src/flir_gige_node.cpp
+++ drivers/flir_gige/src/flir_gige_node.cpp
@@ -5,7 +5,10 @@
 #include <ros/node_handle.h>
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
 #include <dynamic_reconfigure/server.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/core/core.hpp>
 
 #include "flir_gige/gige_camera.h"
 #include "flir_gige/FlirConfig.h"
@@ -20,14 +23,19 @@ class FlirNode {
   image_transport::Publisher image_pub_;
   sensor_msgs::ImagePtr image_msg_;
   dynamic_reconfigure::Server<flir_gige::FlirConfig> server_;
+  ros::Rate rate_;
+  std::string frame_id_;
+  unsigned seq = 0;
   // Flir Camera
   std::shared_ptr<flir_gige::GigeCamera> camera_;
 
  public:
-  FlirNode(const ros::NodeHandle &nh) : nh_{nh}, it_{nh} {
+  FlirNode(const ros::NodeHandle &nh, const double fps)
+      : nh_{nh}, it_{nh}, rate_{fps} {
+    nh_.param<std::string>("frame_id", frame_id_, std::string("flir"));
     // Create a camera
     std::string ip_address;
-    nh_.param("ip_address", ip_address, std::string(""));
+    nh_.param<std::string>("ip_address", ip_address, std::string(""));
     camera_ = std::make_shared<flir_gige::GigeCamera>(ip_address);
     // Setup image publisher and dynamic reconfigure callback
     image_pub_ = it_.advertise("image_raw", 1);
@@ -39,19 +47,35 @@ class FlirNode {
   FlirNode &operator=(const FlirNode &) = delete;  // No assignment operator
 
   bool init() {
-    camera_->FindDevice();
-    camera_->ConnectDevice();
-    camera_->OpenStream();
-    camera_->ConfigureStream();
-    camera_->CreatePipeline();
-    camera_->AcquireImages();
+    //    camera_->FindDevice();
+    //    camera_->ConnectDevice();
+    //    camera_->OpenStream();
+    //    camera_->ConfigureStream();
+    //    camera_->CreatePipeline();
+    //    camera_->AcquireImages();
     return true;
+  }
+
+  void PublishImage(const cv::Mat &image) {
+    if (!ros::ok()) {
+      ros::shutdown();
+    }
+    // Construct a cv image
+    cv_bridge::CvImagePtr cv_ptr;
+    cv_ptr->header.stamp = ros::Time::now();
+    cv_ptr->header.frame_id = frame_id_;
+    cv_ptr->header.seq = seq++;
+    cv_ptr->image = image;
+    cv_ptr->encoding = sensor_msgs::image_encodings::MONO8;
+    // Convert to ros image msg and publish
+    image_msg_ = cv_ptr->toImageMsg();
+    image_pub_.publish(image_msg_);
   }
 
   void ReconfigureCallback(flir_gige::FlirConfig &config, int level) {
     ROS_INFO("%s", "In reconfigure callback");
-    ROS_INFO_STREAM("Level " << level << "Width " << config.width
-                             << " Height " << config.height);
+    ROS_INFO_STREAM("Level " << level << "Width " << config.width << " Height "
+                             << config.height);
   }
 };
 
@@ -61,9 +85,12 @@ int main(int argc, char *argv[]) {
   ros::init(argc, argv, "flir_node");
 
   ros::NodeHandle nh("~");
+  double fps = 0;
+  nh.param<double>("fps", fps, 20);
+
   try {
-    flir_gige::FlirNode fn(nh);
-    fn.init();
+    flir_gige::FlirNode flir_node(nh, fps);
+    flir_node.init();
     ros::spin();
   }
   catch (const std::exception &e) {

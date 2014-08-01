747c8912f3907045fe5556d7dcb6d8eb9d1891f0
Gareth Cross <gcross.code@icloud.com>
2014-08-01 13:55:14 -0400

    Fix for ublox fixer

--- utils/fixbag/fix_ublox.py
+++ utils/fixbag/fix_ublox.py
@@ -19,7 +19,11 @@ class UbloxMessage():
             self.timeOfWeek = msg.iTOW
             # account for atomic offset
             self.timeOfWeek -= 16000
-            # print "navposllh %i" % self.timeOfWeek
+            
+            # clip to time of day
+            dayMillis = 24*3600*1000
+            self.timeOfWeek -= int(self.timeOfWeek / dayMillis)*dayMillis
+            #print "navpos: %i" % self.timeOfWeek
         else:
             # /ublox_gps/fix
             stamp = msg.header.stamp
@@ -34,23 +38,23 @@ class UbloxMessage():
             posix = secs + nsecs / 1.0e9
             D = datetime.fromtimestamp(posix)
 
-            # generate time of week
-            weekday = D.weekday()
+            # generate time of week (day only)
+            #weekday = D.weekday()
             hour = D.hour
             minute = D.minute
             second = D.second
             milli = D.microsecond / 1000
 
             # GPS time starts from sunday
-            weekday = (weekday + 1) % 6
+            #weekday = (weekday + 1) % 6
 
-            tow = (weekday * (3600 * 24) + hour * 3600 +
+            tow = (hour * 3600 +
                    minute * 60 + second) * 1000 + milli
 
             # round to nearest 250ms to match ublox
             tow = int(tow / 250) * 250
             self.timeOfWeek = tow
-
+            #print "fix: %i" % tow
 
 class UbloxFixer():
     fixList = None
@@ -70,46 +74,24 @@ class UbloxFixer():
         elif topic == '/ublox_gps/ublox/navposllh':
             self.navposList.append(UbloxMessage(topic, msg, t))
             self.navposCount += 1
-
-        # replace the smart system with a shitty hack because fuck it
-        converted_list = list()
-        if self.fixList and self.navposList:
-            
-            pairs = (self.fixList[0], self.navposList[0])
-            
-            hAcc = pairs[1].msg.hAcc / 1000.0
-            vAcc = pairs[1].msg.vAcc / 1000.0
-            hMSL = pairs[1].msg.hMSL / 1000.0
-
-            # make a 'covariance' from the accuracies
-            hAcc = (hAcc / 3) * (hAcc / 3)
-            vAcc = (vAcc / 3) * (vAcc / 3)
-
-            pairs[0].msg.altitude = hMSL
-            pairs[0].msg.position_covariance = (
-                hAcc, 0.0, 0.0, 0.0, hAcc, 0.0, 0.0, 0.0, vAcc)
-            converted_list.append((pairs[0].topic, pairs[0].msg))
-            self.outputCount += 1
-            
-            self.fixList = list()
-            self.navposList = list()
-
-        #print converted_list
-        return converted_list
-
-        # check for matches in queue
-        #pub_list = list()
+        
+        # hacky solution, just match greedily at random
+        pub_list = list()
+        for fixMsg in self.fixList:
+            if self.navposList:
+                pub_list.append((fixMsg,self.navposList[0]))
+                self.navposList.remove(self.navposList[0])
+        
         """for fixMsg in self.fixList:
             for navposMsg in self.navposList:
                 print("fix: {0:d}, nav: {1:d}".format(fixMsg.timeOfWeek, navposMsg.timeOfWeek))
                 if fixMsg.timeOfWeek == navposMsg.timeOfWeek:
                     # insert tuple
-                    pub_list.append((fixMsg, navposMsg))
+                    pub_list.append((fixMsg, navposMsg))"""
 
         # remove from the queues
         for pairs in pub_list:
             self.fixList.remove(pairs[0])
-            self.navposList.remove(pairs[1])
 
         # sort in chronometric order
         pub_list = sorted(pub_list, key=lambda pair: pair[0].timeOfWeek)
@@ -126,13 +108,13 @@ class UbloxFixer():
             hAcc = (hAcc / 3) * (hAcc / 3)
             vAcc = (vAcc / 3) * (vAcc / 3)
 
-            pairs[0].msg.altitude = hMSL
+            #pairs[0].msg.altitude = hMSL
             pairs[0].msg.position_covariance = (
                 hAcc, 0.0, 0.0, 0.0, hAcc, 0.0, 0.0, 0.0, vAcc)
             converted_list.append((pairs[0].topic, pairs[0].msg))
             self.outputCount += 1
 
-        return converted_list"""
+        return converted_list
 
 """
 def usage():

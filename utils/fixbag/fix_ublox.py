865c9d3ddd6f11e0592b076b2fc828ef3f663908
Chao Qu <quchao@seas.upenn.edu>
2014-08-01 02:04:46 -0400

    update

--- utils/fixbag/fix_ublox.py
+++ utils/fixbag/fix_ublox.py
@@ -7,143 +7,104 @@ import rosbag
 from datetime import datetime
 from rospy.rostime import Duration
 
+
 class UbloxMessage():
-    def __init__(self,topic,msg,t):
+
+    def __init__(self, topic, msg, t):
         self.topic = topic
         self.msg = msg
         self.t = t
-        
-        if topic=='/ublox_gps/ublox/navposllh':
+
+        if topic == '/ublox_gps/ublox/navposllh':
             self.timeOfWeek = msg.iTOW
             # account for atomic offset
-            self.timeOfWeek = self.timeOfWeek - 16000
-            #print "navposllh %i" % self.timeOfWeek
+            self.timeOfWeek -= 16000
+            # print "navposllh %i" % self.timeOfWeek
         else:
             # /ublox_gps/fix
             stamp = msg.header.stamp
-            
+
             # correct to UTC from EST
-            stamp = stamp + Duration(4*60*60)
-            
+            stamp = stamp + Duration(4 * 60 * 60)
+
             secs = stamp.secs
             nsecs = stamp.nsecs
-            
+
             # convert ROS time to date
-            posix = secs + nsecs/1.0e9            
+            posix = secs + nsecs / 1.0e9
             D = datetime.fromtimestamp(posix)
-            
+
             # generate time of week
             weekday = D.weekday()
             hour = D.hour
             minute = D.minute
             second = D.second
-            milli = D.microsecond/1000;
-            
+            milli = D.microsecond / 1000
+
             # GPS time starts from sunday
             weekday = (weekday + 1) % 6
-            
-            tow = (weekday*(3600*24) + hour*3600 + minute*60 + second)*1000 + milli
-            
+
+            tow = (weekday * (3600 * 24) + hour * 3600 +
+                   minute * 60 + second) * 1000 + milli
+
             # round to nearest 250ms to match ublox
             tow = int(tow / 250) * 250
             self.timeOfWeek = tow
-            
+
+
 class UbloxFixer():
     fixList = None
     navposList = None
     fixCount = 0
     navposCount = 0
     outputCount = 0
-    
+
     def __init__(self):
         self.fixList = list()
         self.navposList = list()
-        
-    def considerMessage(self,topic,msg,t):
+
+    def consider_message(self, topic, msg, t):
         if topic == '/ublox_gps/fix':
-            self.fixList.append(UbloxMessage(topic,msg,t))
-            self.fixCount = self.fixCount+1
+            self.fixList.append(UbloxMessage(topic, msg, t))
+            self.fixCount += 1
         elif topic == '/ublox_gps/ublox/navposllh':
-            self.navposList.append(UbloxMessage(topic,msg,t))
-            self.navposCount = self.navposCount+1
-            
-        pubList = list()
-            
+            self.navposList.append(UbloxMessage(topic, msg, t))
+            self.navposCount += 1
+
+        pub_list = list()
+
         # check for matches in queue
         for fixMsg in self.fixList:
             for navposMsg in self.navposList:
+                print("fix: {0:d}, nav: {1:d}".format(fixMsg.timeOfWeek, navposMsg.timeOfWeek))
                 if fixMsg.timeOfWeek == navposMsg.timeOfWeek:
                     # insert tuple
-                    pubList.append((fixMsg, navposMsg))
-        
+                    pub_list.append((fixMsg, navposMsg))
+
         # remove from the queues
-        for pairs in pubList:
+        for pairs in pub_list:
             self.fixList.remove(pairs[0])
             self.navposList.remove(pairs[1])
-        
+
         # sort in chronometric order
-        pubList = sorted(pubList, key = lambda pair: pair[0].timeOfWeek)
-        
+        pub_list = sorted(pub_list, key=lambda pair: pair[0].timeOfWeek)
+
         # generate covariances and correct height
-        convertedList = list()
-        for pairs in pubList:
+        converted_list = list()
+        for pairs in pub_list:
             # convert to meters
             hAcc = pairs[1].msg.hAcc / 1000.0
             vAcc = pairs[1].msg.vAcc / 1000.0
             hMSL = pairs[1].msg.hMSL / 1000.0
-            
+
             # make a 'covariance' from the accuracies
-            hAcc = (hAcc/3)*(hAcc/3)
-            vAcc = (vAcc/3)*(vAcc/3)
-            
+            hAcc = (hAcc / 3) * (hAcc / 3)
+            vAcc = (vAcc / 3) * (vAcc / 3)
+
             pairs[0].msg.altitude = hMSL
-            pairs[0].msg.position_covariance = (hAcc,0.0,0.0,0.0,hAcc,0.0,0.0,0.0,vAcc)
-            convertedList.append((pairs[0].topic, pairs[0].msg))
-            self.outputCount = self.outputCount + 1
-            
-        return convertedList
-        
-def usage():
-    print("Usage: fixbag --input=<input path> --output=<output path>")
+            pairs[0].msg.position_covariance = (
+                hAcc, 0.0, 0.0, 0.0, hAcc, 0.0, 0.0, 0.0, vAcc)
+            converted_list.append((pairs[0].topic, pairs[0].msg))
+            self.outputCount += 1
 
-def main(argv):
-    inputPath = None
-    outputPath = None
-
-    fixer = UbloxFixer()
-
-    # parse input arguments
-    try:
-        opts, args = getopt.getopt(argv, "h", ['input=', 'output=', 'help'])
-    except getopt.GetoptError:
-        usage()
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif opt == "--input":
-            inputPath = arg
-        elif opt == "--output":
-            outputPath = arg
-
-    if inputPath == None or outputPath == None:
-        usage()
-        sys.exit(3)
-
-    inputPath = os.path.normpath(os.path.expanduser(inputPath))
-    outputPath = os.path.normpath(os.path.expanduser(outputPath))
-
-    # check file paths
-    if not os.path.isfile(inputPath):
-        print "Input path %s is invalid" % (inputPath)
-        sys.exit(4)
-
-    # from the ros wiki:
-    with rosbag.Bag(outputPath, 'w') as outbag:
-        for topic, msg, t in rosbag.Bag(inputPath).read_messages():
-            fixer.considerMessage(topic, msg, t)
-           
-            # TODO: write out to bag file here...
-           
-main(sys.argv[1:])
+        return converted_list
--- utils/fixbag/fixbag.py
+++ utils/fixbag/fixbag.py
@@ -4,8 +4,9 @@ from __future__ import print_function
 import os
 import os.path
 import rosbag
-import rospy
+import sys
 import camera_info_manager
+import fix_ublox
 
 
 def read_left_cinfo():
@@ -15,6 +16,7 @@ def read_left_cinfo():
     return left_cinfo_manager.getCameraInfo()
 
 
+fixer = fix_ublox.UbloxFixer()
 left_cinfo = read_left_cinfo()
 left_image_name = '/mv_stereo/left/image_raw'
 left_cinfo_name = '/mv_stereo/left/camera_info'
@@ -36,28 +38,36 @@ def fix_bag(input_bag, input_path, output_path):
     # Open the bag file
     with rosbag.Bag(output_bag, 'w') as outbag:
         for index, (topic, msg, t) in enumerate(rosbag.Bag(input_bag).read_messages()):
-            if not (index % 10000):
+            # Print progress
+            if not (index % 5000):
                 print(index, end=" ")
+                sys.stdout.flush()
 
             if topic == left_image_name:
                 left_cinfo.header = msg.header
                 # Write left image to outbag
-                outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
+                outbag.write(topic, msg, msg.header.stamp)
                 # Write left cinfo to outbag
-                outbag.write(left_cinfo_name, left_cinfo, left_cinfo.header.stamp if left_cinfo._has_header else t)
+                outbag.write(left_cinfo_name, left_cinfo, left_cinfo.header.stamp)
             elif topic == flir_cinfo_name:
                 # Get and save the first flir camera info
                 if not flir_cinfo:
                     flir_cinfo = msg
             elif topic == flir_image_name:
                 # Write flir camera info with flir image
-                if not flir_cinfo:
+                if flir_cinfo:
                     flir_cinfo.header = msg.header
-                    outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
-                    outbag.write(flir_cinfo_name, flir_cinfo, flir_cinfo.header.stamp if flir_cinfo.header._has_header else t)
+                    outbag.write(topic, msg, msg.header.stamp)
+                    outbag.write(flir_cinfo_name, flir_cinfo, flir_cinfo.header.stamp)
             else:
-                # Write the rest of the topic
-                outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
+                gps_msg_list = fixer.consider_message(topic, msg, t)
+                print(gps_msg_list)
+                if topic != '/ublox_gps/fix':
+                    # Write the rest of the topic
+                    outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
+                else:
+                    for (gps_topic, gps_msg) in gps_msg_list:
+                        outbag.write(gps_topic, gps_msg, gps_msg.header.stamp)
 
 
 

88a33a09fc9326a5cd0da91fe040588665634664
Gareth Cross <gcross.code@icloud.com>
2014-08-13 03:51:24 -0400

    Implementing updatemap

--- core/stereo_vo/src/ceres_bundler.cc
+++ core/stereo_vo/src/ceres_bundler.cc
@@ -197,7 +197,7 @@ void CeresBundler::SolveProblem() {
   options_.num_threads = 1;
   options_.max_solver_time_in_seconds = 10;
   options_.gradient_tolerance = 1e-16;
-  options_.gradient_tolerance = 1e-12;
+  options_.function_tolerance = 1e-12;
   options_.use_inner_iterations = true;
   options_.linear_solver_type = ceres::SPARSE_SCHUR;
   options_.preconditioner_type = ceres::JACOBI;
@@ -212,9 +212,34 @@ void CeresBundler::SolveProblem() {
 
 void CeresBundler::UpdateMap(std::deque<KeyFrame> &key_frames,
                              std::map<Feature::Id,Feature>& features) {
-  
-   
-  
+      
+  //  iterate forwards over keyframes, consider only those in window
+  size_t kf_count=0;
+  for (auto kf_ite = key_frames.begin(); kf_ite != key_frames.end(); 
+       kf_ite++, kf_count++) {
+    
+    if (key_frames.size() - kf_count > win_size_) {
+      continue;
+    }
+    
+    /// @todo: fetch updated keyframe pose here and write back to map
+    Pose kf_pose;
+    
+    for (const Corner& corner : kf_ite->corners()) {
+      //  pull corrected point from results
+      auto p3_ite = point3s_.find(corner.id());
+      ROS_ASSERT_MSG(p3_ite != point3s_.end(), "Point in window that was not optimized!");
+      
+      const Point3Node& p3 = p3_ite->second;
+      kr::vec3<scalar_t> point(p3.ptr()[0],p3.ptr()[1],p3.ptr()[2]);
+      
+      //  back to local pose
+      point = kf_pose.transform(point);
+      
+      Feature& feat = features[corner.id()];
+      feat.set_p_cam(CvPoint3(point[0],point[1],point[2]));
+    }
+  }
 }
 
 }  // namespace stereo_vo

66b371217f99f7db939d8b83783473c08e03e8db
Gareth Cross <gcross.code@icloud.com>
2014-08-20 19:19:15 -0400

    Added G2O to cmake

--- core/stereo_vo/include/stereo_vo/optimizer.h
+++ core/stereo_vo/include/stereo_vo/optimizer.h
@@ -16,6 +16,17 @@
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
 #include <gtsam/inference/Symbol.h>
 
+#include <g2o/core/sparse_optimizer.h>
+#include <g2o/core/block_solver.h>
+#include <g2o/core/solver.h>
+#include <g2o/core/robust_kernel_impl.h>
+#include <g2o/core/optimization_algorithm_levenberg.h>
+#include <g2o/solvers/cholmod/linear_solver_cholmod.h>
+#include <g2o/solvers/csparse/linear_solver_csparse.h>
+#include <g2o/solvers/dense/linear_solver_dense.h>
+#include <g2o/types/sba/types_six_dof_expmap.h>
+#include <g2o/solvers/structure_only/structure_only_solver.h>
+
 #include <image_geometry/stereo_camera_model.h>
 #include "stereo_vo/common.h"
 #include "stereo_vo/pose.h"

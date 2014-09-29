3422bc4b22bf161b4d94fcbc1f2f5e959e004349
Chao Qu <quchao@seas.upenn.edu>
2014-09-29 13:40:09 -0400

    stop pcl_merger and switch to improve pcl2pcd

--- analysis/pcl_merger/include/pcl_merger/pcl_merger_node.hpp
+++ analysis/pcl_merger/include/pcl_merger/pcl_merger_node.hpp
@@ -4,6 +4,7 @@
 #include <ros/ros.h>
 #include <dynamic_reconfigure/server.h>
 #include <pcl_merger/MergerDynConfig.h>
+#include <pcl_ros/point_cloud.h>
 
 namespace pcl_merger {
 
@@ -16,6 +17,8 @@ class PclMergerNode {
 
   ros::NodeHandle nh_;
   dynamic_reconfigure::Server<pcl_merger::MergerDynConfig> cfg_server_;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud1;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud2;
 };
 
 }  // namespace pcl_merger

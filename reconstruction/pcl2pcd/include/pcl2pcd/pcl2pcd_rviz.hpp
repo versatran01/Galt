b302811ede5815eac76fb7063b9d012fc77728ce
Chao Qu <quchao@seas.upenn.edu>
2014-09-29 14:53:26 -0400

    working command from rviz

--- reconstruction/pcl2pcd/include/pcl2pcd/pcl2pcd_rviz.hpp
+++ reconstruction/pcl2pcd/include/pcl2pcd/pcl2pcd_rviz.hpp
@@ -2,14 +2,20 @@
 #define PCL2PCD_RVIZ_HPP_
 
 #include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
 
 namespace pcl2pcd {
 
 class Pcl2PcdRviz {
  public:
-  Pcl2PcdRviz(const ros::NodeHandle &nh);
-private:
+  Pcl2PcdRviz(const ros::NodeHandle& nh);
+
+ private:
+  void GoalCb(const geometry_msgs::PoseStampedConstPtr& pose_stamped);
+
   ros::NodeHandle nh_;
+  ros::Subscriber sub_goal_;
+  ros::Subscriber sub_scan_;
 };
 
 }  // namespace pcl2pcd
--- reconstruction/pcl2pcd/src/pcl2pcd_rviz.cpp
+++ reconstruction/pcl2pcd/src/pcl2pcd_rviz.cpp
@@ -2,6 +2,13 @@
 
 namespace pcl2pcd {
 
-Pcl2PcdRviz::Pcl2PcdRviz(const ros::NodeHandle &nh) {}
+Pcl2PcdRviz::Pcl2PcdRviz(const ros::NodeHandle &nh)
+    : nh_(nh),
+      sub_goal_(nh_.subscribe("/move_base_simple/goal", 1, &Pcl2PcdRviz::GoalCb,
+                              this)) {}
 
+void Pcl2PcdRviz::GoalCb(
+    const geometry_msgs::PoseStampedConstPtr &pose_stamped) {
+  ROS_INFO("Goal recieved");
+}
 }  // namespace pcl2pcd

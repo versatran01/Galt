16ca822d4a10cff95d52d33f7d6b157ca970041c
Chao Qu <quchao@seas.upenn.edu>
2014-09-29 23:31:51 -0400

    fix linker problem with pcl visualizer

--- analysis/pcl_editor/include/pcl_editor/pcl_merger_node.hpp
+++ analysis/pcl_editor/include/pcl_editor/pcl_merger_node.hpp
@@ -5,8 +5,11 @@
 #include <dynamic_reconfigure/server.h>
 #include <pcl_merger/MergerDynConfig.h>
 #include <pcl_ros/point_cloud.h>
+#include <pcl/visualization/pcl_visualizer.h>
 
 namespace pcl_merger {
+
+typedef pcl::PointCloud<pcl::PointWithViewpoint> MyPointCloud;
 
 class PclMergerNode {
  public:
@@ -14,12 +17,16 @@ class PclMergerNode {
 
  private:
   void ConfigCb(const pcl_merger::MergerDynConfig& config, int level);
+  //  void SimpleVis(const MyPointCloud::Ptr& cloud);
 
   ros::NodeHandle nh_;
   dynamic_reconfigure::Server<pcl_merger::MergerDynConfig> cfg_server_;
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud1;
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud2;
+  MyPointCloud::Ptr cloud1_, cloud2_;
+  std::string pcd_merged_;
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer_;
 };
+
+bool LoadPcdFile(const std::string& pcd, const MyPointCloud::Ptr& cloud);
 
 }  // namespace pcl_merger
 
--- analysis/pcl_editor/src/pcl_merger_node.cpp
+++ analysis/pcl_editor/src/pcl_merger_node.cpp
@@ -2,15 +2,46 @@
 
 namespace pcl_merger {
 
-PclMergerNode::PclMergerNode(const ros::NodeHandle& nh) : nh_(nh) {
+PclMergerNode::PclMergerNode(const ros::NodeHandle& nh)
+    : nh_(nh),
+      cloud1_(new MyPointCloud),
+      cloud2_(new MyPointCloud),
+      viewer_(new pcl::visualization::PCLVisualizer("3D Viewer")) {
   cfg_server_.setCallback(boost::bind(&PclMergerNode::ConfigCb, this, _1, _2));
+  std::string pcd1;
+  std::string pcd2;
+  nh_.param<std::string>("pcd1", pcd1, std::string());
+  nh_.param<std::string>("pcd2", pcd2, std::string());
+  nh_.param<std::string>("pcd_merged", pcd_merged_, std::string());
+  // Load two point cloud
+  if (!(LoadPcdFile(pcd1, cloud1_) && LoadPcdFile(pcd2, cloud2_))) {
+    throw std::runtime_error("Failed to load file");
+  }
+  viewer_->setBackgroundColor(0, 0, 0);
+  viewer_->addPointCloud<pcl::PointWithViewpoint>(cloud1_, "cloud1");
+  viewer_->setPointCloudRenderingProperties(
+      pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "cloud1");
+  viewer_->addCoordinateSystem(1.0);
+  viewer_->initCameraParameters();
 }
+
+// void PclMergerNode::SimpleVis(const pcl::PointCloud::Ptr& cloud) {}
 
 void PclMergerNode::ConfigCb(const MergerDynConfig& config, int level) {
   if (level < 0) {
     ROS_INFO("Initialize reconfigure server for %s",
              nh_.getNamespace().c_str());
   }
+}
+
+bool LoadPcdFile(const std::string& pcd, const MyPointCloud::Ptr& cloud) {
+  if (pcl::io::loadPCDFile<pcl::PointWithViewpoint>(pcd, *cloud) == -1) {
+    PCL_ERROR("Couldn't read file %s \n", pcd.c_str());
+    return false;
+  }
+  ROS_INFO("Loaded %d points from %s", int(cloud->width * cloud->height),
+           pcd.c_str());
+  return true;
 }
 
 }  // namespace pcl_merge

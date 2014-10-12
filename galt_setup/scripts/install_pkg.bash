ba9eed02e507599350c45aa5d8f933af4bd52148
Chao Qu <quchao@seas.upenn.edu>
2014-10-11 20:38:29 -0400

    shell script is :poop:

---
+++ galt_setup/scripts/install_pkg.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# This file will install all dependencies from apt-get
+PACAKGES=(
+autojump
+bpython
+calibre
+clang
+cmake
+compiz
+compizconfig-settings-manager
+compiz-plugins
+curl
+dstat
+exuberant-ctags
+git
+gitg
+git-review
+guake
+htop
+meld
+pkg-config
+python-matplotlib
+python-numpy
+python-pip
+python-scipy
+tig
+tmux
+valgrind
+vim
+vim-gtk
+)
+
+#Stop if a command ends in an error
+set -e
+
+#Check to see if we are running with root privileges
+if [[ $(id -u) -ne 0 ]] ; then
+    echo "Please run this script as root (eg using sudo)"
+    exit 1
+fi
+
+echo "Making sure all system software is up to date."
+apt-get update --force-yes
+apt-get -y upgrade --force-yes
+
+#Intall packages
+apt-get -y --force-yes install ${PACKAGES[@]}

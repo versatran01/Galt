e68ba68c7f7d7ffd05c9afdc4ceb5df7db01d4a1
Chao Qu <quchao@seas.upenn.edu>
2014-10-18 14:46:09 -0400

    add some stuff to setup script

--- galt_setup/profiles/aerial/v1/sensor.bash
+++ galt_setup/profiles/aerial/v1/sensor.bash
@@ -1,7 +1,7 @@
 # This file will set the following environmental variable to be read by launch
 # files.
 export GALT_PLATFORM_NAME=aerial
-export GALT_PLATFORM_VER=1
+export GALT_PLATFORM_VER=v1
 #All cameras
 export GALT_CAMERA_CALIB_DIR=package://galt_setup/profiles/${GALT_PLATFORM_NAME}/${GALT_PLATFORM_VER}/calib/camera
 
@@ -21,14 +21,14 @@ export GALT_LASER_IP=192.168.0.10
 export GALT_LASER_FRAME=laser
 
 #IMU
-export GALT_IMU_SERIAL=usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6234.21073-if00
+export GALT_IMU_SERIAL=usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6234.38162-if00
 export GALT_IMU_RATE=250
 export GALT_IMU_FRAME=imu
 
 #GPS
-export GALT_GPS_SERIAL=usb-u-blox_AG_-_www.u-blox.com_u-blox_7_-_GPS_GNSS_Receiver-if00
+export GALT_GPS_SERIAL=usb-u-blox_AG_-_www.u-blox.com_u-blox_GNSS_receiver-if00
 export GALT_GPS_RATE=10
-export GALT_GPS_VERSION=7
+export GALT_GPS_VERSION=8
 export GALT_GPS_ENABLE_PPP=false
 export GALT_GPS_ENABLE_GLONASS=false
 export GALT_GPS_FRAME=gps

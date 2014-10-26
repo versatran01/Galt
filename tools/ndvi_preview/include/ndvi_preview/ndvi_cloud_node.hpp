d4ad887aa6611db1790341bc435cd8fbc37252d3
Chao Qu <quchao@seas.upenn.edu>
2014-10-26 14:58:39 -0400

    generate ndvi cloud

--- tools/ndvi_preview/include/ndvi_preview/ndvi_cloud_node.hpp
+++ tools/ndvi_preview/include/ndvi_preview/ndvi_cloud_node.hpp
@@ -4,7 +4,9 @@
 #include <ros/ros.h>
 
 #include <laser_assembler/AssembleScans2.h>
+#include <std_srvs/Empty.h>
 #include <ndvi_preview/SaveToPcd.h>
+#include <ndvi_preview/LoadFromPcd.h>
 
 namespace ndvi_preview {
 
@@ -14,12 +16,16 @@ class NdviCloudNode {
       : nh_(nh),
         save_srv_server_(nh_.advertiseService("save_to_pcd",
                                               &NdviCloudNode::SaveToPcd, this)),
+        ndvi_srv_server_(nh_.advertiseService(
+            "generate_ndvi", &NdviCloudNode::GenerateNdvi, this)),
         assemble_red_srv_client_(
             nh_.serviceClient<laser_assembler::AssembleScans2>("assemble_red")),
         assemble_ir_srv_client_(
-            nh_.serviceClient<laser_assembler::AssembleScans2>("assemble_ir")) {
-  }
+            nh_.serviceClient<laser_assembler::AssembleScans2>("assemble_ir")),
+        ndvi_pub_(nh_.advertise<sensor_msgs::PointCloud2>("ndvi", 1, true)) {}
 
+  bool GenerateNdvi(std_srvs::Empty::Request &req,
+                    std_srvs::Empty::Response &res);
   bool SaveToPcd(SaveToPcd::Request &req, SaveToPcd::Response &res);
   bool AssembleCloud2(laser_assembler::AssembleScans2 &srv,
                       ros::ServiceClient &client);
@@ -29,8 +35,10 @@ class NdviCloudNode {
  private:
   ros::NodeHandle nh_;
   ros::ServiceServer save_srv_server_;
+  ros::ServiceServer ndvi_srv_server_;
   ros::ServiceClient assemble_red_srv_client_;
   ros::ServiceClient assemble_ir_srv_client_;
+  ros::Publisher ndvi_pub_;
 };
 
 }  // namespace ndvi
--- tools/ndvi_preview/src/ndvi_cloud_node.cpp
+++ tools/ndvi_preview/src/ndvi_cloud_node.cpp
@@ -1,11 +1,70 @@
 #include <ros/ros.h>
 #include <laser_assembler/AssembleScans2.h>
+#include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/kdtree/kdtree_flann.h>
 
 #include "ndvi_preview/ndvi_cloud_node.hpp"
 
 namespace ndvi_preview {
+
+bool NdviCloudNode::GenerateNdvi(std_srvs::Empty::Request &req,
+                                 std_srvs::Empty::Response &res) {
+  laser_assembler::AssembleScans2 srv_red, srv_ir;
+  // Call two services to get assembled red and ir point clouds
+  if (AssembleCloud2(srv_red, assemble_red_srv_client_) &&
+      AssembleCloud2(srv_ir, assemble_ir_srv_client_)) {
+    const int K = 1;
+
+    // Convert to pcl point cloud type
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_red(
+        new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_ir(
+        new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_ndvi(
+        new pcl::PointCloud<pcl::PointXYZRGB>);
+    // cloud ndvi should have the same size as cloud red
+    cloud_ndvi->height = 1;
+    cloud_ndvi->points.reserve(cloud_red->width);
+    pcl_conversions::toPCL(srv_red.response.cloud.header, cloud_ndvi->header);
+
+    pcl::fromROSMsg(srv_red.response.cloud, *cloud_red);
+    pcl::fromROSMsg(srv_ir.response.cloud, *cloud_ir);
+
+    // Iterate through all points in red and find the 1nn in ir
+    pcl::KdTreeFLANN<pcl::PointXYZRGB> kdtree;
+    kdtree.setInputCloud(cloud_ir);
+    for (size_t i = 0; i < cloud_red->width; ++i) {
+      const pcl::PointXYZRGB &red_point = cloud_red->points[i];
+      std::vector<int> point_1nn(K);
+      std::vector<float> point_1nn_dist2(K);
+      if (kdtree.nearestKSearch(red_point, K, point_1nn, point_1nn_dist2)) {
+        const pcl::PointXYZRGB &ir_point = cloud_ir->points[i];
+        pcl::PointXYZRGB ndvi_point(red_point);
+        auto ndvi = static_cast<float>(ir_point.r - red_point.r) /
+                    (ir_point.r + red_point.r);
+        ndvi = (ndvi + 1) / 2 * 255;
+        auto ndvi_rgb = static_cast<uint8_t>(ndvi);
+        ndvi_point.r = ndvi_rgb;
+        ndvi_point.g = ndvi_rgb;
+        ndvi_point.b = ndvi_rgb;
+        cloud_ndvi->points.push_back(ndvi_point);
+        cloud_ndvi->width += 1;
+      }
+    }
+
+    ROS_INFO_STREAM("number of ndvi: " << cloud_ndvi->width
+                                       << " number of red: "
+                                       << cloud_red->width);
+    // Convert ndvi cloud to PointCloud2 and publish
+    sensor_msgs::PointCloud2 cloud_ndvi_msg;
+    pcl::toROSMsg(*cloud_ndvi, cloud_ndvi_msg);
+    ndvi_pub_.publish(cloud_ndvi_msg);
+    return true;
+  }
+  return false;
+}
 
 bool NdviCloudNode::SaveToPcd(SaveToPcd::Request &req,
                               SaveToPcd::Response &res) {
@@ -19,7 +78,7 @@ bool NdviCloudNode::SaveToPcd(SaveToPcd::Request &req,
       return res.success;
     }
     res.success = false;
-    ROS_INFO("Something bad happend");
+    ROS_INFO("Failed to save to pcd");
     return res.success;
   }
 }
@@ -49,6 +108,7 @@ bool NdviCloudNode::SavePcdToDir(const sensor_msgs::PointCloud2 &cloud_ros,
   pcl::PointCloud<pcl::PointXYZRGB> cloud_pcl_w;
   pcl::fromROSMsg(cloud_ros, cloud_pcl_w);
   std::string full_filename = dirname + "/" + filename;
+
   try {
     ROS_INFO_STREAM("Saving cloud to " << full_filename);
     pcl::io::savePCDFile(full_filename, cloud_pcl_w);
@@ -58,6 +118,7 @@ bool NdviCloudNode::SavePcdToDir(const sensor_msgs::PointCloud2 &cloud_ros,
     ROS_ERROR("%s: %s", nh_.getNamespace().c_str(), e.what());
     return false;
   }
+
   return true;
 }
 

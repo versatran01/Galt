7fcc21224fce236c4c9d162421f663e0fb34ef2d
Chao Qu <quchao@seas.upenn.edu>
2015-01-11 22:02:30 -0500

    refactored img2pcl not using laser assembler for faster response

--- reconstruction/img2pcl/include/img2pcl/img2pcl_node.h
+++ reconstruction/img2pcl/include/img2pcl/img2pcl_node.h
@@ -3,73 +3,86 @@
 
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
+#include <image_geometry/pinhole_camera_model.h>
+#include <cv_bridge/cv_bridge.h>
+
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/PointCloud2.h>
-#include <image_geometry/pinhole_camera_model.h>
+
 #include <tf2_ros/transform_listener.h>
 #include <eigen_conversions/eigen_msg.h>
 #include <eigen3/Eigen/Geometry>
+
 #include <pcl_ros/point_cloud.h>
 #include <pcl/common/transforms.h>
 
 #include <opencv2/highgui/highgui.hpp>
 
+#include <boost/optional.hpp>
+
 namespace galt {
 namespace img2pcl {
 
 class Img2pclNode {
-
  public:
   Img2pclNode(const ros::NodeHandle &nh, const ros::NodeHandle &pnh);
 
- private:
-  constexpr static double kDelay = 0.0;
-
   void CameraCb(const sensor_msgs::ImageConstPtr &image_msg,
                 const sensor_msgs::CameraInfoConstPtr &cinfo_msg);
+  void Cloud2Cb(const sensor_msgs::PointCloud2ConstPtr &cloud2_msg);
   void ConnectCb();
-  bool GetLatestTransfrom(const std::string &frame_tgt,
-                          const std::string &frame_src,
-                          geometry_msgs::TransformStamped *tf_stamped) const;
 
+ private:
+  /**
+   * @brief GetLatestTransfrom Get latest transfrom from src to tgt
+   * @param frame_tgt The frame to which data should be transformed
+   * @param frame_src The frame in which data origniated
+   * @return transform stamped if not none
+   */
+  boost::optional<geometry_msgs::TransformStamped> GetLatestTransfrom(
+      const std::string &frame_tgt, const std::string &frame_src) const;
+
+  int queue_size_;
   ros::NodeHandle nh_, pnh_;
   image_transport::ImageTransport it_;
   image_transport::CameraSubscriber sub_camera_;
+  ros::Subscriber sub_cloud2_;
   image_geometry::PinholeCameraModel camera_model_;
-  ros::Publisher pub_cloud_;
-  ros::ServiceClient srv_client_;
+  boost::mutex connect_mutex_;
+  ros::Publisher pub_cloud2_;
   tf2::BufferCore core_;
   tf2_ros::TransformListener tf_listener_;
+  cv_bridge::CvImageConstPtr cv_ptr_;
 };
 
-template <typename PclPointT, typename Scalar>
-void CloudToPoints3(const pcl::PointCloud<PclPointT> &pcl_cloud,
-                    std::vector<cv::Point3_<Scalar>> *cv_pts) {
+pcl::PointCloud<pcl::PointXYZ> TransformCloud(
+    const pcl::PointCloud<pcl::PointXYZ> &pcl_cloud_src,
+    const geometry_msgs::TransformStamped &transform_stamped);
+
+pcl::PointCloud<pcl::PointXYZRGB> ExtractColor(
+    const pcl::PointCloud<pcl::PointXYZ> &pcl_cloud_src,
+    const pcl::PointCloud<pcl::PointXYZ> &pcl_cloud_tgt, const cv::Mat &image,
+    const image_geometry::PinholeCameraModel &model);
+
+template <typename PclPointT>
+std::vector<cv::Point3f> CloudToCvPoints3(
+    const pcl::PointCloud<PclPointT> &pcl_cloud) {
+  std::vector<cv::Point3f> cv_pts;
   for (const PclPointT &point : pcl_cloud.points) {
-    cv_pts->emplace_back(point.x, point.y, point.z);
+    cv_pts.emplace_back(point.x, point.y, point.z);
   }
+  return cv_pts;
 }
 
 template <typename Scalar>
-bool IsInsideImage(cv::Point_<Scalar> point, int width, int height) {
-  return (point.x >= 0) && (point.y >= 0) && (point.x < width) &&
-         (point.y < height);
+bool IsInsideImage(const cv::Point_<Scalar> &point, const cv::Size &size) {
+  return (point.x >= 0) && (point.y >= 0) && (point.x < size.width) &&
+         (point.y < size.height);
 }
 
-void TransformCloud(const sensor_msgs::PointCloud2 &src_ros_cloud,
-                    const geometry_msgs::Transform &transform,
-                    const std::string &tgt_frame,
-                    pcl::PointCloud<pcl::PointXYZ> *tgt_pcl_cloud);
-
-void ExtractInfoFromImage(const pcl::PointCloud<pcl::PointXYZ> &pcl_cloud_in1,
-                          const pcl::PointCloud<pcl::PointXYZ> &pcl_cloud_in2,
-                          const cv::Mat &image,
-                          const image_geometry::PinholeCameraModel &model,
-                          sensor_msgs::PointCloud2 *ros_cloud_out1);
-
-}  // namespace thermal_map
+}  // namespace img2pcl
 }  // namespace galt
 
-#endif  // GALT_THERMAL_MAP_NODE_H_
+#endif  // GALT_IMG2PCL_NODE_H_
--- reconstruction/img2pcl/src/img2pcl_main.cpp
+++ reconstruction/img2pcl/src/img2pcl_main.cpp
@@ -2,11 +2,15 @@
 
 #include <ros/ros.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   ros::init(argc, argv, "img2pcl");
   ros::NodeHandle nh;
   ros::NodeHandle pnh("~");
 
-  galt::img2pcl::Img2pclNode img2pcl_node(nh, pnh);
-  ros::spin();
+  try {
+    galt::img2pcl::Img2pclNode img2pcl_node(nh, pnh);
+    ros::spin();
+  } catch (const std::exception& e) {
+    ROS_ERROR("%s: %s", pnh.getNamespace().c_str(), e.what());
+  }
 }

da91e20de9ab9a5785be0108908d00ea70761c81
Chao Qu <quchao@seas.upenn.edu>
2015-02-23 19:58:21 -0500

    some problem with connect_cb

--- tools/ndvi_preview/include/ndvi_preview/ndvi_preview_node.hpp
+++ tools/ndvi_preview/include/ndvi_preview/ndvi_preview_node.hpp
@@ -1,5 +1,53 @@
-#ifndef NDVI_PREVIEW_NODE_HPP
-#define NDVI_PREVIEW_NODE_HPP
+#ifndef GALT_NDVI_PREVIEW_NODE_HPP_
+#define GALT_NDVI_PREVIEW_NODE_HPP_
 
-#endif // NDVI_PREVIEW_NODE_HPP
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <sensor_msgs/CameraInfo.h>
 
+namespace ndvi_preview {
+
+using sensor_msgs::ImageConstPtr;
+using sensor_msgs::CameraInfoConstPtr;
+using message_filters::sync_policies::ApproximateTime;
+using ImageSubscriberFilter = image_transport::SubscriberFilter;
+using CinfoSubscriberFilter =
+    message_filters::Subscriber<sensor_msgs::CameraInfo>;
+using ApproximatePolicy =
+    ApproximateTime<sensor_msgs::Image, sensor_msgs::CameraInfo,
+                    sensor_msgs::Image, sensor_msgs::CameraInfo>;
+using ApproximateSync = message_filters::Synchronizer<ApproximatePolicy>;
+
+class NdviPreviewNode {
+ public:
+  explicit NdviPreviewNode(const ros::NodeHandle& pnh);
+
+  void ConnectCb();
+  void SyncedCameraCb(const ImageConstPtr& nir_image_msg,
+                      const CameraInfoConstPtr& nir_cinfo_msg,
+                      const ImageConstPtr& red_image_msg,
+                      const CameraInfoConstPtr& red_cinfo_msg);
+
+ private:
+  void SubscribeSyncedTopics();
+  void SubscribeSingleCamera(const std::string& camera,
+                             ImageSubscriberFilter& sub_image,
+                             CinfoSubscriberFilter& sub_cinfo);
+
+  ros::NodeHandle pnh_;
+  image_transport::ImageTransport it_;
+  ImageSubscriberFilter sub_image_nir_, sub_image_red_;
+  CinfoSubscriberFilter sub_cinfo_nir_, sub_cinfo_red_;
+  boost::shared_ptr<ApproximateSync> approximate_sync_;
+  image_transport::Publisher pub_image_ndvi_;
+  boost::mutex connect_mutex_;
+  int queue_size_;
+};
+
+}  // namespace ndvi_preview
+
+#endif  // GALT_NDVI_PREVIEW_NODE_HPP_
--- tools/ndvi_preview/src/ndvi_preview_node.cpp
+++ tools/ndvi_preview/src/ndvi_preview_node.cpp
@@ -9,152 +9,184 @@
  *      Author: gareth
  */
 
+#include "ndvi_preview/ndvi_preview_node.hpp"
+
+#include <boost/thread/lock_guard.hpp>
+
 #include <iostream>
 #include <string>
 #include <algorithm>
 
-#include <ros/ros.h>
-
 #include <cv_bridge/cv_bridge.h>
-#include <opencv2/opencv.hpp>
+#include <sensor_msgs/image_encodings.h>
 
-#include <image_transport/image_transport.h>
-#include <image_transport/subscriber_filter.h>
-#include <message_filters/subscriber.h>
-#include <message_filters/time_synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
+namespace ndvi_preview {
 
-#define QUEUE_SIZE (10)
+NdviPreviewNode::NdviPreviewNode(const ros::NodeHandle &pnh)
+    : pnh_(pnh), it_(pnh) {
+  pnh_.param<int>("queue_size", queue_size_, 5);
 
-int save_number = 0;
+  approximate_sync_.reset(new ApproximateSync(ApproximatePolicy(queue_size_),
+                                              sub_image_nir_, sub_cinfo_nir_,
+                                              sub_image_red_, sub_cinfo_red_));
+  approximate_sync_->registerCallback(
+      boost::bind(&NdviPreviewNode::SyncedCameraCb, this, _1, _2, _3, _4));
 
-void image_callback(const sensor_msgs::ImageConstPtr &irImage,
-                    const sensor_msgs::CameraInfoConstPtr &irInfo,
-                    const sensor_msgs::ImageConstPtr &redImage,
-                    const sensor_msgs::CameraInfoConstPtr &redInfo) {
-  cv_bridge::CvImageConstPtr bridgedIrPtr =
-      cv_bridge::toCvShare(irImage, "mono8");
-  cv_bridge::CvImageConstPtr bridgedRedPtr =
-      cv_bridge::toCvShare(redImage, "mono8");
+  image_transport::SubscriberStatusCallback connect_cb =
+      boost::bind(&NdviPreviewNode::ConnectCb, this);
 
-  if (!bridgedIrPtr || !bridgedRedPtr) {
-    ROS_ERROR("Failed to convert image to mono8 with cv_bridge");
-    return;
-  }
+  boost::lock_guard<boost::mutex> lock(connect_mutex_);
+  pub_image_ndvi_ = it_.advertise("ndvi", 1, connect_cb, connect_cb);
+}
 
-  cv::Mat outputIr(bridgedIrPtr->image.size(), CV_8UC1);
-  cv::Mat adjustedIr(bridgedIrPtr->image.size(), CV_8UC1, cv::Scalar(0, 0, 0));
-  cv::Mat adjustedRed(bridgedIrPtr->image.size(), CV_8UC1, cv::Scalar(0, 0, 0));
-  cv::Mat NDVI(bridgedIrPtr->image.size(), CV_8UC1, cv::Scalar(0, 0, 0));
+void NdviPreviewNode::SubscribeSyncedTopics() {
+  SubscribeSingleCamera("nir", sub_image_nir_, sub_cinfo_nir_);
+  SubscribeSingleCamera("red", sub_image_red_, sub_cinfo_red_);
+}
 
-  //  NOTE: This is all a harcoded mess for now, just for demo purposes...
-  const int dispx = 8;
-  const int dispy = 18;
+void NdviPreviewNode::SubscribeSingleCamera(const std::string &camera,
+                                            ImageSubscriberFilter &sub_image,
+                                            CinfoSubscriberFilter &sub_cinfo) {
+  image_transport::TransportHints hints("raw", ros::TransportHints(), pnh_);
+  ros::NodeHandle cnh(pnh_, camera);
+  sub_image.subscribe(it_, ros::names::append(camera, "image"), queue_size_,
+                      hints);
+  sub_cinfo.subscribe(cnh, "camera_info", queue_size_);
+}
 
-  //  correct the IR image for circular error, then convert to reflectance
-  //  and calculate an NDVI
-  for (int y = 0; y < bridgedIrPtr->image.rows; y++) {
-    for (int x = 0; x < bridgedIrPtr->image.cols; x++) {
-      const double delx = x - 674;
-      const double dely = y - 459;
-
-      const double rad = std::sqrt(delx * delx + dely * dely);
-      const double scl = -2e-6 * (rad * rad) + 0.0001716 * rad + 0.9508;
-
-      const uchar irIn = bridgedIrPtr->image.at<uchar>(y, x);
-
-      int red_x = x + dispx;
-      int red_y = y + dispy;
-
-      red_x = std::min(std::max(red_x, 0), bridgedRedPtr->image.cols);
-      red_y = std::min(std::max(red_y, 0), bridgedRedPtr->image.rows);
-
-      const uchar redIn = bridgedRedPtr->image.at<uchar>(red_y, red_x);
-
-      uchar irOut;
-
-      if (rad < 450) {
-        irOut = static_cast<uchar>(std::min(255.0, irIn / scl));
-
-        //  convert to reflectance
-        double ir = irIn / scl;
-        ir = ir / 255.0;
-        ir = std::min(std::max(0.6759 * ir - 0.00971, 0.0), 1.0);
-
-        double red = redIn / 255.0;
-        red = std::min(std::max(1.296 * red - 0.07744, 0.0), 1.0);
-
-        double ndvi = (ir - red) / (ir + red);
-        ndvi = (ndvi + 1) / 2;
-        // ndvi = std::min(std::max(ndvi,0.0),1.0);
-
-        if (ndvi > 1.0 || ndvi < 0.0) {
-          ROS_ERROR("bad calc!");
-        }
-
-        adjustedIr.at<uchar>(y, x) = static_cast<uchar>(ir * 255);
-        adjustedRed.at<uchar>(y, x) = static_cast<uchar>(red * 255);
-        NDVI.at<uchar>(y, x) = static_cast<uchar>(ndvi * 255);
-
-      } else {
-        irOut = 0;
-      }
-
-      outputIr.at<uchar>(y, x) = irOut;
-    }
-  }
-
-  cv::imshow("IR", outputIr);
-  cv::imshow("Red", bridgedRedPtr->image);
-  cv::imshow("Calibrated IR", adjustedIr);
-  cv::imshow("Calibrated Red", adjustedRed);
-  cv::imshow("NDVI", NDVI);
-
-  if (cv::waitKey(30) == static_cast<int>('s')) {
-    //  save images
-    cv::imwrite("output_ir_" + std::to_string(save_number) + ".png",
-                bridgedIrPtr->image);
-    cv::imwrite("output_red_" + std::to_string(save_number) + ".png",
-                bridgedRedPtr->image);
-
-    save_number++;
+void NdviPreviewNode::ConnectCb() {
+  boost::lock_guard<boost::mutex> lock(connect_mutex_);
+  ROS_INFO("Num subscribers: %d", (int)pub_image_ndvi_.getNumSubscribers());
+  if (pub_image_ndvi_.getNumSubscribers() == 0) {
+    sub_image_nir_.unsubscribe();
+    sub_cinfo_nir_.unsubscribe();
+    sub_image_red_.unsubscribe();
+    sub_cinfo_red_.unsubscribe();
+    ROS_INFO("Unsubscribe!");
+  } else if (!sub_image_nir_.getSubscriber()) {
+    SubscribeSyncedTopics();
+    ROS_INFO("Subscribe!");
   }
 }
 
+void NdviPreviewNode::SyncedCameraCb(const ImageConstPtr &nir_image_msg,
+                                     const CameraInfoConstPtr &nir_cinfo_msg,
+                                     const ImageConstPtr &red_image_msg,
+                                     const CameraInfoConstPtr &red_cinfo_msg) {
+  ROS_WARN_THROTTLE(1, "IN!!");
+  const cv::Mat nir_image = cv_bridge::toCvShare(nir_image_msg)->image;
+  const cv::Mat red_image = cv_bridge::toCvShare(red_image_msg)->image;
+}
+
+}  // namespace ndvi_preview
+
+//#define QUEUE_SIZE (10)
+
+// int save_number = 0;
+
+// void image_callback(const sensor_msgs::ImageConstPtr &irImage,
+//                    const sensor_msgs::CameraInfoConstPtr &irInfo,
+//                    const sensor_msgs::ImageConstPtr &redImage,
+//                    const sensor_msgs::CameraInfoConstPtr &redInfo) {
+//  cv_bridge::CvImageConstPtr bridgedIrPtr =
+//      cv_bridge::toCvShare(irImage, "mono8");
+//  cv_bridge::CvImageConstPtr bridgedRedPtr =
+//      cv_bridge::toCvShare(redImage, "mono8");
+
+//  if (!bridgedIrPtr || !bridgedRedPtr) {
+//    ROS_ERROR("Failed to convert image to mono8 with cv_bridge");
+//    return;
+//  }
+
+//  cv::Mat outputIr(bridgedIrPtr->image.size(), CV_8UC1);
+//  cv::Mat adjustedIr(bridgedIrPtr->image.size(), CV_8UC1, cv::Scalar(0, 0,
+//  0));
+//  cv::Mat adjustedRed(bridgedIrPtr->image.size(), CV_8UC1, cv::Scalar(0, 0,
+//  0));
+//  cv::Mat NDVI(bridgedIrPtr->image.size(), CV_8UC1, cv::Scalar(0, 0, 0));
+
+//  //  NOTE: This is all a harcoded mess for now, just for demo purposes...
+//  const int dispx = 8;
+//  const int dispy = 18;
+
+//  //  correct the IR image for circular error, then convert to reflectance
+//  //  and calculate an NDVI
+//  for (int y = 0; y < bridgedIrPtr->image.rows; y++) {
+//    for (int x = 0; x < bridgedIrPtr->image.cols; x++) {
+//      const double delx = x - 674;
+//      const double dely = y - 459;
+
+//      const double rad = std::sqrt(delx * delx + dely * dely);
+//      const double scl = -2e-6 * (rad * rad) + 0.0001716 * rad + 0.9508;
+
+//      const uchar irIn = bridgedIrPtr->image.at<uchar>(y, x);
+
+//      int red_x = x + dispx;
+//      int red_y = y + dispy;
+
+//      red_x = std::min(std::max(red_x, 0), bridgedRedPtr->image.cols);
+//      red_y = std::min(std::max(red_y, 0), bridgedRedPtr->image.rows);
+
+//      const uchar redIn = bridgedRedPtr->image.at<uchar>(red_y, red_x);
+
+//      uchar irOut;
+
+//      if (rad < 450) {
+//        irOut = static_cast<uchar>(std::min(255.0, irIn / scl));
+
+//        //  convert to reflectance
+//        double ir = irIn / scl;
+//        ir = ir / 255.0;
+//        ir = std::min(std::max(0.6759 * ir - 0.00971, 0.0), 1.0);
+
+//        double red = redIn / 255.0;
+//        red = std::min(std::max(1.296 * red - 0.07744, 0.0), 1.0);
+
+//        double ndvi = (ir - red) / (ir + red);
+//        ndvi = (ndvi + 1) / 2;
+//        // ndvi = std::min(std::max(ndvi,0.0),1.0);
+
+//        if (ndvi > 1.0 || ndvi < 0.0) {
+//          ROS_ERROR("bad calc!");
+//        }
+
+//        adjustedIr.at<uchar>(y, x) = static_cast<uchar>(ir * 255);
+//        adjustedRed.at<uchar>(y, x) = static_cast<uchar>(red * 255);
+//        NDVI.at<uchar>(y, x) = static_cast<uchar>(ndvi * 255);
+
+//      } else {
+//        irOut = 0;
+//      }
+
+//      outputIr.at<uchar>(y, x) = irOut;
+//    }
+//  }
+
+//  cv::imshow("IR", outputIr);
+//  cv::imshow("Red", bridgedRedPtr->image);
+//  cv::imshow("Calibrated IR", adjustedIr);
+//  cv::imshow("Calibrated Red", adjustedRed);
+//  cv::imshow("NDVI", NDVI);
+
+//  if (cv::waitKey(30) == static_cast<int>('s')) {
+//    //  save images
+//    cv::imwrite("output_ir_" + std::to_string(save_number) + ".png",
+//                bridgedIrPtr->image);
+//    cv::imwrite("output_red_" + std::to_string(save_number) + ".png",
+//                bridgedRedPtr->image);
+
+//    save_number++;
+//  }
+//}
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "ndvi_preview_node");
+  ros::NodeHandle pnh("~");
 
-  ros::NodeHandle nh("~");
-  image_transport::ImageTransport imgTransport(nh);
-
-  image_transport::SubscriberFilter subIRImage;
-  image_transport::SubscriberFilter subRedImage;
-
-  message_filters::Subscriber<sensor_msgs::CameraInfo> subIRInfo;
-  message_filters::Subscriber<sensor_msgs::CameraInfo> subRedInfo;
-
-  subIRImage.subscribe(imgTransport, "infrared/image_raw", QUEUE_SIZE);
-  subRedImage.subscribe(imgTransport, "red/image_raw", QUEUE_SIZE);
-
-  subIRInfo.subscribe(nh, "infrared/camera_info", QUEUE_SIZE);
-  subRedInfo.subscribe(nh, "red/camera_info", QUEUE_SIZE);
-
-  //  time sync policy
-  typedef message_filters::sync_policies::ApproximateTime<
-      sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::Image,
-      sensor_msgs::CameraInfo> TimeSyncPolicy;
-
-  message_filters::Synchronizer<TimeSyncPolicy> sync(TimeSyncPolicy(QUEUE_SIZE),
-                                                     subIRImage, subIRInfo,
-                                                     subRedImage, subRedInfo);
-  sync.registerCallback(boost::bind(&image_callback, _1, _2, _3, _4));
-
-  cv::namedWindow("IR", cv::WINDOW_NORMAL);
-  cv::namedWindow("Red", cv::WINDOW_NORMAL);
-  cv::namedWindow("Calibrated IR", cv::WINDOW_NORMAL);
-  cv::namedWindow("Calibrated Red", cv::WINDOW_NORMAL);
-  cv::namedWindow("NDVI", cv::WINDOW_NORMAL);
-
-  ros::spin();
-  return 0;
+  try {
+    ndvi_preview::NdviPreviewNode ndvi_preview_node(pnh);
+    ros::spin();
+  } catch (const std::exception &e) {
+    ROS_ERROR("%s: %s", pnh.getNamespace().c_str(), e.what());
+  }
 }

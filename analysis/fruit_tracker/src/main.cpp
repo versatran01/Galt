b3e61f394b72e149902e5c2594870e8b0e8e1fe0
Gareth Cross <gcross.code@icloud.com>
2015-03-13 11:07:13 -0400

    Nuked crapped fruit tracker

--- analysis/fruit_tracker/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <fruit_tracker/node.hpp>
-
-int main(int argc, char ** argv) {
-  ros::init(argc,argv,"fruit_tracker");
-  ros::NodeHandle pnh("~");
-  galt::fruit_tracker::Node node(pnh);
-  ros::spin();
-  return 0;
-}

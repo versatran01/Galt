fe7adf4fc479d5e2edbb658fc0860f9689b59130
Chao Qu <quchao@seas.upenn.edu>
2015-03-19 12:18:52 -0400

    realized dont need message filters

--- tools/ndvi_preview/src/extract_keyframe.cpp
+++ tools/ndvi_preview/src/extract_keyframe.cpp
@@ -10,6 +10,7 @@
 #include <message_filters/time_synchronizer.h>
 
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 template <class M>
 class BagSubscriber : public message_filters::SimpleFilter<M> {
@@ -21,16 +22,21 @@ class BagSubscriber : public message_filters::SimpleFilter<M> {
 
 class KeyframeExtractor {
  public:
-  KeyframeExtractor() = default;
+  KeyframeExtractor(const ros::NodeHandle& pnh) : pnh_(pnh) {
+    pnh_.param("scale", scale_, 0.5);
+  }
 
-  void CameraCb(const sensor_msgs::Image::ConstPtr& image_msg,
-                const sensor_msgs::CameraInfo::ConstPtr& cinfo_msg) {
+  void ImageCb(const sensor_msgs::Image::ConstPtr& image_msg) {
     const auto image = cv_bridge::toCvShare(image_msg)->image;
-    cv::imshow("image", image);
+    cv::Mat image_scaled;
+    cv::resize(image, image_scaled, cv::Size(0, 0), 0.5, 0.5);
+    cv::imshow("image", image_scaled);
     cv::waitKey(1);
   }
 
  private:
+  ros::NodeHandle pnh_;
+  double scale_;
 };
 
 //  _                _
@@ -60,22 +66,14 @@ int main(int argc, char** argv) {
   std::string camera_ns;
   pnh.param<std::string>("camera", camera_ns, "/spectral_670");
   const std::string image_topic = camera_ns + "/image_rect";
-  const std::string cinfo_topic = camera_ns + "/camera_info";
-  const std::vector<std::string> camera_topics = {image_topic, cinfo_topic};
 
   // Open rosbag
   rosbag::Bag in_bag(in_bag_path);
-  rosbag::View view(in_bag, rosbag::TopicQuery(camera_topics));
+  rosbag::View view(in_bag, rosbag::TopicQuery(image_topic));
 
   ROS_INFO("Opened bag: %s", in_bag.getFileName().c_str());
 
-  KeyframeExtractor extractor;
-  BagSubscriber<sensor_msgs::Image> sub_image;
-  BagSubscriber<sensor_msgs::CameraInfo> sub_cinfo;
-  message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::CameraInfo>
-      sync(sub_image, sub_cinfo, 25);
-  sync.registerCallback(
-      boost::bind(&KeyframeExtractor::CameraCb, &extractor, _1, _2));
+  KeyframeExtractor extractor(pnh);
 
   for (auto it = view.begin(), it_end = view.end(); it != it_end; ++it) {
     const rosbag::MessageInstance& m = *it;
@@ -84,14 +82,7 @@ int main(int argc, char** argv) {
     if (m.getTopic() == image_topic || ("/" + m.getTopic() == image_topic)) {
       sensor_msgs::ImageConstPtr image_msg =
           m.instantiate<sensor_msgs::Image>();
-      if (image_msg) sub_image.newMessage(image_msg);
-    }
-
-    // CameraInfo topic
-    if (m.getTopic() == cinfo_topic || ("/" + m.getTopic() == cinfo_topic)) {
-      sensor_msgs::CameraInfoConstPtr cinfo_msg =
-          m.instantiate<sensor_msgs::CameraInfo>();
-      if (cinfo_msg) sub_cinfo.newMessage(cinfo_msg);
+      if (image_msg) extractor.ImageCb(image_msg);
     }
   }
 }

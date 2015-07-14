346f3d18db6b683410b26165e1549a495579afc5
Anurag Makineni <anurag.makineni@gmail.com>
2015-07-14 17:54:59 -0400

    implemented image callback to highlight saturated areas

--- analysis/led/src/led_node.cpp
+++ analysis/led/src/led_node.cpp
@@ -3,6 +3,7 @@
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 namespace led {
 
@@ -15,15 +16,29 @@ class Node {
   ros::NodeHandle pnh_;
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
+  int threshold_;
 };
 
 Node::Node(const ros::NodeHandle& pnh) : pnh_(pnh), it_(pnh) {
   image_sub_ = it_.subscribe("image", 1, &Node::imageCb, this);
+
+  //get param for threshold
+  pnh.param("threshold",threshold_, 250);
 }
 
 void Node::imageCb(const sensor_msgs::ImageConstPtr& image_msg) {
   const auto image = cv_bridge::toCvCopy(image_msg)->image;
-  cv::imshow("image", image);
+
+  //convert to rgb
+  cv::Mat image_rgb(image.size(), CV_8UC3);
+  cv::cvtColor(image,image_rgb,CV_GRAY2RGB);
+
+  //create mask and apply threshold
+  cv::threshold(image,image,threshold_,255,0);
+  image_rgb.setTo(cv::Scalar(51,51,255),image);
+
+  //display image
+  cv::imshow("Saturation Highlighted", image_rgb);
   cv::waitKey(1);
 }
 

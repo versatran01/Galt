6d3e6b3eadf06b235f1365537495721fbeef6caa
Anurag Makineni <anurag.makineni@gmail.com>
2015-07-17 13:07:04 -0400

    moved away from cv image viewer to publishing a topic

--- analysis/led/scripts/intensity_profiler.py
+++ analysis/led/scripts/intensity_profiler.py
@@ -15,7 +15,7 @@ class IntensityProfiler:
     def __init__(self):
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("image", Image, self.image_callback)
-        cv2.namedWindow("Live Image", 1)
+        self.image_pub = rospy.Publisher("intensity_profiler/image_drawn",Image,queue_size=1)
         plt.ion()
 
     def image_callback(self, data):
@@ -30,39 +30,35 @@ class IntensityProfiler:
         height, width, channels = cv_image.shape;
         # convert to np array
         np_image = np.asarray(cv_image_gray)
-        np_image[0:10,:] = 0
+        np_image[0:15,:] = 0 #to get rid of bad pixels
 
         # find max values and draw lines on image
         itemindex = np.where(np_image == np.amax(np_image))
         x_max = int(np.median(itemindex[1]))
         y_max = int(np.median(itemindex[0]))
 
+        #plot and draw line on image
         vertical = rospy.get_param("~vertical")
-
         plt.cla()
-
         if vertical:
             plt.plot(np_image[:, x_max])
             cv2.line(cv_image, (x_max, 0), (x_max, height), (51, 51, 255), 2)
         else:
             plt.plot(np_image[y_max, :])
             cv2.line(cv_image, (0, y_max), (width, y_max), (51, 51, 255), 2)
-
         plt.ylabel("Intensity")
         plt.xlabel("Distance on Profile (p)")
         plt.ylim([0, 255])
         plt.title("Intensity Profile")
-
         fig = plt.gcf()
         fig.canvas.set_window_title('Intensity Profile')
-
         plt.draw()
-        plt.pause(1.0 / 1000)
 
-        # show image
-        cv2.imshow("Live Image", cv_image)
-        cv2.waitKey(1)
-
+        #publish
+        try:
+            self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+        except CvBridgeError, e:
+            print(e)
 
 def main(args):
     profiler = IntensityProfiler()

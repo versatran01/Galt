6f3146610b4a74142403238649d1a16037683a76
Chao Qu <quchao@seas.upenn.edu>
2015-12-10 00:00:46 -0500

    format

--- detection/aye/tune.py
+++ detection/aye/tune.py
@@ -9,7 +9,8 @@ def tune_svc(X, y):
     params = [
         {'kernel': ['rbf'], 'C': [0.01, 0.1, 1, 10, 100]}]
 
-    grid = grid_search.GridSearchCV(estimator=svm.SVC(), param_grid=params, cv=5)
+    grid = grid_search.GridSearchCV(estimator=svm.SVC(), param_grid=params,
+                                    cv=5)
     grid.fit(X, y)
 
     return grid
@@ -17,7 +18,8 @@ def tune_svc(X, y):
 
 def tune_lr(X, y):
     params = {'C': [0.1, 1, 10, 50, 100]}
-    grid = grid_search.GridSearchCV(estimator=linear_model.LogisticRegression(), param_grid=params, cv=5)
+    grid = grid_search.GridSearchCV(estimator=linear_model.LogisticRegression(),
+                                    param_grid=params, cv=5)
     grid.fit(X, y)
 
     return grid
@@ -25,7 +27,8 @@ def tune_lr(X, y):
 
 def tune_rf(X, y):
     params = {'n_estimators': [10, 50, 200]}
-    grid = grid_search.GridSearchCV(estimator=ensemble.RandomForestClassifier(), param_grid=params, cv=5)
+    grid = grid_search.GridSearchCV(estimator=ensemble.RandomForestClassifier(),
+                                    param_grid=params, cv=5)
     grid.fit(X, y)
 
     return grid
@@ -37,9 +40,12 @@ def tune_ensemble(X, y):
     clf_lr = linear_model.LogisticRegression()
     clf_rf = ensemble.RandomForestClassifier()
 
-    eclf = ensemble.VotingClassifier(estimators=[('lr', clf_lr), ('svc', clf_svc), ('gnb', clf_gnb), ('rf', clf_rf)],
-                                     voting='hard')
-    params = {'svc__kernel': ['rbf'], 'svc__C': [0.01, 0.1, 1, 10, 100], 'lr__C': [1, 100],
+    eclf = ensemble.VotingClassifier(
+        estimators=[('lr', clf_lr), ('svc', clf_svc), ('gnb', clf_gnb),
+                    ('rf', clf_rf)],
+        voting='hard')
+    params = {'svc__kernel': ['rbf'], 'svc__C': [0.01, 0.1, 1, 10, 100],
+              'lr__C': [1, 100],
               'rf__n_estimators': [20, 200]}
 
     grid = grid_search.GridSearchCV(estimator=eclf, param_grid=params, cv=5)

85df09ad28ec6cf63bbbe13b618a4bbc7ed0dbbf
Chao Qu <quchao@seas.upenn.edu>
2015-12-10 21:37:56 -0500

    add kalman filter code

---
+++ detection/aye/kalman_filter.py
@@ -0,0 +1,15 @@
+from __future__ import print_function, division, absolute_import
+
+
+class KalmanFilter(object):
+    def __init__(self):
+        self.P
+        self.p
+        self.v
+        pass
+
+    def predict(self):
+        pass
+
+    def correct(self, y):
+        pass

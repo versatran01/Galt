d9ab1d9eaea1f00b3e263357af21a38876eb706a
Chao Qu <quchao@seas.upenn.edu>
2015-12-11 19:59:03 -0500

    prototyping optical flow tracking

--- detection/aye/detector.py
+++ detection/aye/detector.py
@@ -1,3 +1,16 @@
 from __future__ import print_function, division, absolute_import
+from aye.preprocessing import Samples
 
-bagfile = '/home/chao/Workspace/bag/frame/rect_fixed/frame1_rect_fixed.bag'
+
+class Detector(object):
+    def __init__(self, clf, scaler):
+        self.clf = clf
+        self.scaler = scaler
+
+    def detect(self, im_bgr):
+        s = Samples(im_bgr)
+        X = self.scaler.transform(s.X())
+        y = self.clf.predict(X)
+        bw = s.y_to_bw(y, to_gray=True)
+        # TODO: should bw goes into Samples?
+        return s, bw

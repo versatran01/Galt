d6dc00104130a2e4dae5f6439da58e9ef7c9c3ca
Anurag Makineni <anurag.makineni@gmail.com>
2016-02-05 13:22:31 -0500

    added multiple kernels, still needs work

--- detection/scpye/scpye/test_gabor.py
+++ detection/scpye/scpye/test_gabor.py
@@ -12,25 +12,46 @@ import cv2
 
 from skimage.filters import gabor_kernel, gabor_filter
 from scpye.image_pipeline import ImagePipeline
+import matplotlib.pyplot as plt
 # %%
 
 dr = DataReader('/home/anuragmakineni/Desktop/', color='green', mode='slow_flash')
 image = dr.load_image(2)
-imshow(image)
+plt.imshow(image)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 #%%
 frequency = 0.1
-theta = 45
-
+theta = 0
 theta = (theta / 4.0) * np.pi
-kernel = np.real(gabor_kernel(frequency, theta))
-imshow(kernel)
+
 #%%
-real, imag = gabor_filter(gray, frequency, theta)
-imshow(real)
-#%%
+real_0, imag_0 = gabor_filter(gray, frequency, theta)
+theta = 45
+theta = (theta / 4.0) * np.pi
+
+imshow(real_0)
+
+real_45, imag_45 = gabor_filter(gray, frequency, theta)
+theta = 90
+theta = (theta / 4.0) * np.pi
+
+imshow(real_45)
+
+real_90, imag_90 = gabor_filter(gray, frequency, theta)
+theta = 135
+theta = (theta / 4.0) * np.pi
+
+imshow(real_90)
 
 
+real_135, imag_135 = gabor_filter(gray, frequency, theta)
+
+imshow(real_135)
 
 
+filtered = real_0 + real_45 + real_90 + real_135
+
+filtered = cv2.normalize(filtered)
+
+plt.imshow(filtered)

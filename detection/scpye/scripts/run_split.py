310b0390b1575777c0ebf53231b1cbebfe12432e
Chao Qu <quchao@seas.upenn.edu>
2016-02-15 13:03:27 -0500

    update

--- detection/scpye/scripts/run_split.py
+++ detection/scpye/scripts/run_split.py
@@ -6,12 +6,16 @@ Created on Thu Feb 11 21:34:34 2016
 """
 
 # %% 
+import os
 import cv2
 import numpy as np
 from scpye.data_reader import DataReader
 from scpye.fruit_detector import FruitDetector
+from scpye.region_props import find_contours
 from scpye.blob_analyzer import *
 from scpye.visualization import *
+
+save_dir = '/home/chao/Desktop'
 
 # %%
 # Raw detection
@@ -29,6 +33,8 @@ fd = FruitDetector.from_pickle(dr.model_dir)
 I = dr.load_image(index)
 bw = fd.detect(I)
 
+#cv2.imwrite(os.path.join(save_dir, color + '_raw.png'), fd.color)
+
 # %%
 # Raw classifier detection results
 min_area = 5
@@ -41,10 +47,12 @@ disp_left = fd.color
 disp_right = fd.color
 disp_right[bw_clean == 0] = 0
 
-draw_contours(disp_left, cntrs)
-draw_bboxes(disp_right, blobs['bbox'])
+draw_contours(disp_left, cntrs, color=(0, 0, 255))
+draw_bboxes(disp_right, blobs['bbox'], color=(0, 0, 255))
 
 imshow2(disp_left, disp_right)
+#cv2.imwrite(os.path.join(save_dir, color + '_cntr.png'), disp_left)
+#cv2.imwrite(os.path.join(save_dir, color + '_bbox.png'), disp_right)
 
 # %%
 # Find what is likely to be multiple fruits
@@ -52,7 +60,7 @@ disp = fd.color
 
 blobs_multi = []
 cntrs_multi = []
-
+cntrs_single = []
 areas = blobs['prop'][:, 0]
 area_thresh = np.mean(areas)
 for blob, cntr in zip(blobs, cntrs):
@@ -63,12 +71,15 @@ for blob, cntr in zip(blobs, cntrs):
         blobs_multi.append(blob)
         cntrs_multi.append(cntr)
     else:
-        draw_contour(disp, cntr)
+        draw_contour(disp, cntr, color=(0, 0, 255))
+        cntrs_single.append(cntr)
 imshow(disp)
+#cv2.imwrite(os.path.join(save_dir, color + '_multi.png'), disp)
 
 # %%
 # For each candidate find max points and split the blob
 disp_left = fd.color
+draw_contours(disp_left, cntrs_single, color=(0, 0, 255))
 v = fd.v
 v[bw_clean == 0] = 0
 for blob, cntr in zip(blobs_multi, cntrs_multi):
@@ -82,9 +93,17 @@ for blob, cntr in zip(blobs_multi, cntrs_multi):
                                indices=False, exclude_border=False)
     marker = ndi.label(local_max, structure=np.ones((3, 3)))[0]
     label = watershed(-image_max, marker, mask=bw_bbox)
+    n = np.max(label)
     local_max = gray_from_bw(local_max)
     points = local_max_points(local_max)
     if points is not None and len(points) > 1:
-        draw_points(bgr, points)
-        imshow3(bgr, image_max, label)
+        for i in np.arange(1, n + 1):
+            mask = np.array(label == i, np.uint8)
+            cs = find_contours(mask)
+            draw_contour(bgr, cs[0], color=(0, 255, 255))
+                        
+            
+        draw_points(bgr, points, radius=2, color=(0, 255, 255))
+        
 imshow(disp_left)
+cv2.imwrite(os.path.join(save_dir, color + '_split.png'), disp_left)
